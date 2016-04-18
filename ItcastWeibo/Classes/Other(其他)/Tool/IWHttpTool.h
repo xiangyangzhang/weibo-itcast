c4b0acfb20703981a15d6a60e485c70dc664b770
xiangyangzhang <xiangyanga23@163.com>
2016-04-18 16:41:30 +0800

    f

---
+++ ItcastWeibo/Classes/Other(其他)/Tool/IWHttpTool.h
@@ -0,0 +1,13 @@
+//
+//  IWHttpTool.h
+//  ItcastWeibo
+//
+//  Created by apple on 14-5-19.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IWHttpTool : NSObject
+
+@end
---
+++ ItcastWeibo/Classes/Other(其他)/Tool/IWHttpTool.m
@@ -0,0 +1,13 @@
+//
+//  IWHttpTool.m
+//  ItcastWeibo
+//
+//  Created by apple on 14-5-19.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import "IWHttpTool.h"
+
+@implementation IWHttpTool
+
+@end
